711642046aba69494d25c9a66aa5096b7909eddd
Nafie <warrior259@gmail.com>
2024-05-11 17:37:50 +0300

    chore: add app entrypoint

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using MongoDB.Driver;
+using System.Net;
+using AppLogger;
+using API.Auth;
+using API.Clan.Info;
+using API.Clan.Join;
+using API.Clan.Leave;
+using API.Clan.PointsS;
+using Schema.Clan;
+using Schema.User;
+using MongoDB.Bson.Serialization;
+
+
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+string CorsMiddlewareName = "OriginsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CorsMiddlewareName, policy => { policy.WithOrigins(["http://127.0.0.1:3000", "http://localhost:3000"]).AllowAnyHeader(); });
+});
+builder.Logging.AddConsole();
+WebApplication app = builder.Build();
+IMongoDatabase database = Database.Database.MongoDatabase();
+ILogger logger = Logger.GetLogger();
+var UserDocument = database.GetCollection<UserSchema>("user");
+var ClanDocument = database.GetCollection<ClanSchema>("clan");
+
+using (StreamReader r = new StreamReader("clans.json"))
+{
+    string json = r.ReadToEnd();
+    List<ClanSchema> rawQuery = BsonSerializer.Deserialize<List<ClanSchema>>(json);
+    ClanDocument.InsertMany(rawQuery);
+}
+
+app.UseCors(CorsMiddlewareName);
+app.UseWhen(context => context.Request.Path.StartsWithSegments("/clan"), appBuilder =>
+{
+    appBuilder.Use(async (context, next) =>
+    {
+        if (!context.Request.Headers.ContainsKey("authorization"))
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            await context.Response.WriteAsync("Unauthorized");
+            return;
+        }
+        string username = context.Request.Headers.Authorization.ToString();
+        long count = await UserDocument.CountDocumentsAsync(Builders<UserSchema>.Filter.Eq("name", username));
+        if (count == 0)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            await context.Response.WriteAsync("Unauthorized");
+            return;
+        }
+        await next.Invoke();
+    });
+});
+app.RegisterLoginEndpoint();
+app.RegisterGetClansEndpoint();
+app.RegisterGetClanByIdEndpoint();
+app.RegisterJoinClanByIdEndpoint();
+app.RegisterLeaveClanByIdEndpoint();
+app.RegisterAddPointsToClanByIdEndpoint();
+app.RegisterSubtractPointsToClanByIdEndpoint();
+app.RegisterSetPointsToClanByIdEndpoint();
+
+app.Run();
