17085f9000269b257f47d354f535374eea661db3
Nafie <warrior259@gmail.com>
2024-05-13 07:29:59 +0300

    chore: reuse db connection

--- db.cs
+++ db.cs
@@ -1,5 +1,4 @@
 using AppLogger;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Database
@@ -10,16 +9,25 @@ namespace Database
         public static string DatabaseName = "ClanSystemDB";
         public static string ClanCollectionName = "clan";
         public static string UserCollectionName = "user";
+        public static MongoClient Client { get; set; }
         public static IMongoDatabase? MongoDatabase()
         {
             logger.LogInformation("Attempting to connect to Database");
-            string? DB_URL = Environment.GetEnvironmentVariable("DB_URL");
-            if (string.IsNullOrWhiteSpace(DB_URL))
+            try
             {
-                throw new ArgumentException("Invalid DB URL");
+                string? DB_URL = Environment.GetEnvironmentVariable("DB_URL");
+                if (string.IsNullOrWhiteSpace(DB_URL))
+                {
+                    throw new ArgumentException("Invalid DB URL");
+                }
+                logger.LogInformation("Connecting to " + DB_URL);
+                Client ??= new(DB_URL);
+                return Client.GetDatabase("ClanSystemDB");
             }
-            MongoClient client = new(DB_URL);
-            return client.GetDatabase(DatabaseName);
+            catch (Exception ex)
+            {
+                throw new Exception("Could not connect to database, reason: " + ex.Message);
+            }
         }
     }
 }
